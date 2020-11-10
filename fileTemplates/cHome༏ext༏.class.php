d2b283aff36a033b635ec92f6d39be7dfa9605d0
咸光金 <yong531315@gmail.com>
2020-11-11 00:12:44 +0800

    PY-2019.3.1 <ysnows@ysnows.local Create markdown.xml, IntelliLang.xml, project.default.xml, diff.xml, Enum.java, Default copy.xml, Interface.java, Default.xml, find.xml, AndroidComments.xml, code.style.schemes.xml, java.xml, path.macros.xml, Darcula copy.icls, CustomView.java, thinkphp.xml, hg.xml, Default.xml, PHP.xml, Model.java, Python.xml, Android.xml, keymap.xml, editor.xml, applicationLibraries.xml, webServers.xml, debugger.xml, updates.xml, baseRefactoring.xml, vim_settings.xml, ide.general.xml, rainbow_brackets.xml, .DS_Store, Singleton.java, Default copy 0.xml, user.xml, git.xml, terminal.xml, ui.lnf.xml, filetypes.xml, remote-servers.xml, Adapter.java, plugin_ui.xml, colors.scheme.xml, html_xml.xml, notifications.xml, databaseDrivers.xml, github.xml, templates.xml, Vue.xml, AnnotationType.java, emmet.xml, parameter.hints.xml, Class.java, jdk.table.xml, mHome༏ext༏.class.php, laf.xml, Default _1_.xml, Mac OS X 10_5_ copy.xml, web-browsers.xml, proxy.settings.xml, cHome༏ext༏.class.php, vcs.xml, databaseSettings.xml

---
+++ fileTemplates/cHome༏ext༏.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ysnows
+ * Date: 2017/7/18
+ * Time: 下午4:37
+ */
+
+namespace Home\Controller;
+
+class ${NAME} extends BaseController
+{
+
+    public function index()
+    {
+
+    }
+
+}
---
+++ fileTemplates/mHome༏ext༏.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ysnows
+ * Date: 2017/7/18
+ * Time: 下午4:37
+ */
+
+namespace Home\Model;
+
+class ${NAME} extends BaseModel
+{
+
+    
+
+}
